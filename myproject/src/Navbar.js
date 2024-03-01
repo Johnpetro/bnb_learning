3240c651cb9ea96efc222d5070710134d72aed97
john petro <johnpetro335@gmail.com>
2024-03-02 02:20:15 +0300

    my first commit

---
+++ myproject/src/Navbar.js
@@ -0,0 +1,27 @@
+import airbnb from './air.png';
+const Navbar = ()=>{
+    
+    return (
+        <div className="nav">
+        <div className="navbar">
+            <div className="log">
+            <img src={airbnb}alt="Image"style={
+                {width:"160px",}
+            } />
+            </div>
+            <div className="menu">
+               <div>
+               <ul> 
+                    <li>home</li>
+                    <li>contact</li>
+                    <li>about us</li> 
+                </ul>
+               </div>
+            </div>
+            <div className="left-bar">2sdjsfjdfjdjfkjsdkfjk</div>
+
+        </div>
+    </div>
+    );
+}
+export default Navbar;
